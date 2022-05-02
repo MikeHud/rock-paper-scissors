455ef7bce5dd2a397df19d070623b7a6cdc99bd1
Mike Hudson <mikehudson106@gmail.com>
2022-05-02 07:15:37 +0100

    Inital functionality of game complete

--- rps-game.js
+++ rps-game.js
@@ -1,10 +1,52 @@
+const sel = ["rock", "paper", "scissors"];
 function computerPlay() {
-        
-    const computerSel = ["rock", "paper", "scissors"];
-    
-    //return selection[0];
-    console.log(computerSel[1]);
-    
+    return sel[Math.floor(Math.random() * 3)];
 }
 
-computerPlay();
+let userSel = parseInt(prompt("select 1.Rock 2.Paper 3.Scissors"));
+const computerSel = computerPlay();
+
+function playRound(computer, user) {
+    let result = "";
+    switch(user) {
+        case 1:
+            if(computer == "rock") {     
+                result = "you chose " + user + "\n" + "computer chose " + computer + "\n" + "It's a draw";
+                return result;
+            } else {
+                result = "you chose " + user + "\n" + "computer chose " + computer + "\n" + "You win";
+                return result;
+            }
+            
+        break;
+        case 2:
+            if(computer == "paper") {
+                result = "you chose " + user + "\n" + "computer chose " + computer + "\n" + "It's a draw";
+                return result;
+            } else if(computer == "rock") {
+                result = "you chose " + user + "\n" + "computer chose " + computer + "\n" + "You win";
+                return result;
+            } else if(computer == "scissors") {
+                result = "you chose " + user + "\n" + "computer chose " + computer + "\n" + "You lose";
+                return result;
+            }
+
+        break;
+        case 3:
+            if(computer == "scissors") {
+                result = "you chose " + user + "\n" + "computer chose " + computer + "\n" + "It's a draw";
+                return result;
+            } else if(computer == "rock") {
+                result = "you chose " + user + "\n" + "computer chose " + computer + "\n" + "You lose";
+                return result;
+            } else if(computer == "paper") {
+                result = "you chose " + user + "\n" + "computer chose " + computer + "\n" + "You win";
+                return result;
+            }
+
+        break;
+    }
+
+}
+
+console.log(playRound(computerSel, userSel));
