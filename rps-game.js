0618bac525fad618b26f82f164851fe50caefc40
Mike Hudson <mikehudson106@gmail.com>
2022-05-02 07:15:37 +0100

    Use function for displaying repetitive strings in results

--- rps-game.js
+++ rps-game.js
@@ -6,39 +6,43 @@ function computerPlay() {
 let userSel = parseInt(prompt("select 1.Rock 2.Paper 3.Scissors"));
 const computerSel = computerPlay();
 
+function displayResults(computer,user) {
+    return "you chose " + sel[user - 1] + "\n" + "computer chose " + computer + "\n"
+}
+
 function playRound(computer, user) {
     let result = "";
     switch(user) {
         case 1:
             if(computer == "rock") {     
-                result = "you chose " + sel[user - 1] + "\n" + "computer chose " + computer + "\n" + "It's a draw";
+                result = displayResults(computer,user) + "It's a draw";
                 return result;
             } else {
-                result = "you chose " + sel[user - 1] + "\n" + "computer chose " + computer + "\n" + "You win";
+                result = displayResults(computer,user) + "You win";
                 return result;
             }
         break;
         case 2:
             if(computer == "paper") {
-                result = "you chose " + sel[user - 1] + "\n" + "computer chose " + computer + "\n" + "It's a draw";
+                result = displayResults(computer,user)+ "It's a draw";
                 return result;
             } else if(computer == "rock") {
-                result = "you chose " + sel[user - 1] + "\n" + "computer chose " + computer + "\n" + "You win";
+                result = displayResults(computer,user) + "You win";
                 return result;
             } else if(computer == "scissors") {
-                result = "you chose " + sel[user - 1] + "\n" + "computer chose " + computer + "\n" + "You lose";
+                result = displayResults(computer,user) + "You lose";
                 return result;
             }
         break;
         case 3:
             if(computer == "scissors") {
-                result = "you chose " + sel[user - 1] + "\n" + "computer chose " + computer + "\n" + "It's a draw";
+                result = displayResults(computer,user) + "It's a draw";
                 return result;
             } else if(computer == "rock") {
-                result = "you chose " + sel[user - 1] + "\n" + "computer chose " + computer + "\n" + "You lose";
+                result = displayResults(computer,user) + "You lose";
                 return result;
             } else if(computer == "paper") {
-                result = "you chose " + sel[user - 1] + "\n" + "computer chose " + computer + "\n" + "You win";
+                result = displayResults(computer,user) + "You win";
                 return result;
             }
         break;
