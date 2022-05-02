b4f550dc92cfb369513bf21b9bc56b3a7ab862b6
Mike Hudson <mikehudson106@gmail.com>
2022-05-02 09:01:26 +0100

    initial commit: project skeleton

---
+++ rps-game.js
@@ -0,0 +1,10 @@
+function computerPlay() {
+        
+    const computerSel = ["rock", "paper", "scissors"];
+    
+    //return selection[0];
+    console.log(computerSel[1]);
+    
+}
+
+computerPlay();
