87f1878991eb832ec501099135ba4607116be699
Mike Hudson <mikehudson106@gmail.com>
2022-05-02 21:34:05 +0100

    store win, lose, draw messages in variables

--- rps-game.js
+++ rps-game.js
@@ -1,12 +1,15 @@
 const sel = ["rock", "paper", "scissors"];
+let userSel = parseInt(prompt("select 1.Rock 2.Paper 3.Scissors"));
+const computerSel = computerPlay();
+const winMsg = "You win!";
+const loseMsg = "You lose!";
+const drawMsg = "It's a draw";
+
 function computerPlay() {
     return sel[Math.floor(Math.random() * 3)];
 }
 
-let userSel = parseInt(prompt("select 1.Rock 2.Paper 3.Scissors"));
-const computerSel = computerPlay();
-
-function displayResults(computer,user) {
+function displayResults(computer,user) { 
     return "you chose " + sel[user - 1] + "\n" + "computer chose " + computer + "\n"
 }
 
@@ -15,34 +18,34 @@ function playRound(computer, user) {
     switch(user) {
         case 1:
             if(computer == "rock") {     
-                result = displayResults(computer,user) + "It's a draw";
+                result = displayResults(computer,user) + drawMsg;
                 return result;
             } else {
-                result = displayResults(computer,user) + "You win";
+                result = displayResults(computer,user) + winMsg;
                 return result;
             }
         break;
         case 2:
             if(computer == "paper") {
-                result = displayResults(computer,user)+ "It's a draw";
+                result = displayResults(computer,user)+ drawMsg;
                 return result;
             } else if(computer == "rock") {
-                result = displayResults(computer,user) + "You win";
+                result = displayResults(computer,user) + winMsg;
                 return result;
             } else if(computer == "scissors") {
-                result = displayResults(computer,user) + "You lose";
+                result = displayResults(computer,user) + loseMsg;
                 return result;
             }
         break;
         case 3:
             if(computer == "scissors") {
-                result = displayResults(computer,user) + "It's a draw";
+                result = displayResults(computer,user) + drawMsg;
                 return result;
             } else if(computer == "rock") {
-                result = displayResults(computer,user) + "You lose";
+                result = displayResults(computer,user) + loseMsg;
                 return result;
             } else if(computer == "paper") {
-                result = displayResults(computer,user) + "You win";
+                result = displayResults(computer,user) + winMsg;
                 return result;
             }
         break;
