fdb7810392fe976b0a805fed26e99cc0cf190021
Mike Hudson <mikehudson106@gmail.com>
2022-05-03 08:32:00 +0100

    Implement besst of 5 rounds announcing score and winner at the end

--- rps-game.js
+++ rps-game.js
@@ -1,11 +1,8 @@
 const sel = ["rock", "paper", "scissors"];
-let userSel = prompt("type rock, paper or scissors").toLowerCase();
-const computerSel = computerPlay();
 const winMsg = "You win!";
 const loseMsg = "You lose!";
 const drawMsg = "It's a draw!";
 
-console.log(userSel);
 function computerPlay() {
     return sel[Math.floor(Math.random() * 3)];
 }
@@ -47,7 +44,43 @@ function playRound(computer, user) {
             }
         break;
     }
+}
+/* Best of 5 rounds */
+function game() {
+let userScore = 0;
+let computerScore = 0;
+let game = [""]; //store the 5 rounds in an array 
 
+    for (let i = 1; i <= 5; i++) {
+    console.log("Round" + i); //display round count 
+    userSel = prompt("type rock, paper or scissors").toLowerCase(); // get new user input
+    game[i-1] = playRound(computerPlay(), userSel); // run game i
+        if (game[i-1].includes("win")) {
+            console.log(game[i-1]);
+            userScore = userScore + 1;
+            
+        } else if (game[i-1].includes("lose")) {
+            console.log(game[i-1]);
+            computerScore = computerScore + 1;
+            
+        } else if (game[i-1].includes("draw")) {
+            console.log(game[i-1]);
+        }
+
+        if (i == 5) {
+            console.log("You score " + userScore);
+            console.log("computer scores " + computerScore);
+
+            if(computerScore > userScore) {
+                console.log("---Computer wins that round---");
+            } else if (computerScore < userScore) {
+                console.log("---You win that round---");
+            } else {
+                console.log("---Looks like draw---");
+            }
+        }
+    } 
+    
 }
 
-console.log(playRound(computerSel, userSel));
+game();
