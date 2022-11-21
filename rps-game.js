a7c77f780b5efd419240e81c4b12590053852952
Mike Hudson <mikehudson106@gmail.com>
2022-11-21 11:46:36 +0000

    Change game logic to first to reach 5 points

--- rps-game.js
+++ rps-game.js
@@ -4,6 +4,7 @@ const loseMsg = "You lose!";
 const drawMsg = "It's a draw!";
 let userSel = "";
 let result = "";
+let roundCount = 0;
 
 let userScore = 0;
 let computerScore = 0;
@@ -39,41 +40,50 @@ function playRound(computer, user) { //game logic. One round
             if(computer == "rock") {     
                 result = user + " " + "draws with " + computer;
                 displayResult(result); 
+                roundCount++;
             } else if(computer == "paper") {
                 result = computer + " " + "beats " + user;
                 computerScore++;
+                roundCount++;
                 displayResult(result);
             } else if(computer == "scissors") {
                 result = user + " " + "beats " + computer;
                 userScore++;
+                roundCount++;
                 displayResult(result);
             }
         break;
         case "paper":
             if(computer == "paper") {
                 result = user + " " + "draws with " + computer;
+                roundCount++;
                 displayResult(result);
             } else if(computer == "rock") {
                 result = user + " " + "beats " + computer;
                 userScore++;
+                roundCount++;
                 displayResult(result);
             } else if(computer == "scissors") {
                 result = computer + " " + "beats " + user;
-                computerScore++;;
+                computerScore++;
+                roundCount++;
                 displayResult(result);
             }
         break;
         case "scissors":
             if(computer == "scissors") {
                 result = drawMsg + " " + user + " " + "draws with " + computer;
+                roundCount++;
                 displayResult(result);
             } else if(computer == "rock") {
                 result = loseMsg + " " + computer + " " + "beats " + user;
                 computerScore++;
+                roundCount++;
                 displayResult(result);
             } else if(computer == "paper") {
                 result = winMsg + " " + user + " " + "beats " + computer;
                 userScore++;
+                roundCount++;
                 displayResult(result);
             }  
         break;  
@@ -83,7 +93,7 @@ function displayResult(resultString) { //display scores and winner of 5 rounds
     document.getElementById("round-result").innerHTML = result;
     document.getElementById("user-score").innerHTML = userScore;
     document.getElementById("computer-score").innerHTML = computerScore;
-    if(computerScore === 5 || userScore === 5) {
+    if(roundCount >= 5) {
         if(userScore > computerScore) {
             document.getElementById("display-winner").innerHTML = "<span style= 'color: green'>You Win!</span>";
         } else if(userScore < computerScore) {
@@ -93,6 +103,7 @@ function displayResult(resultString) { //display scores and winner of 5 rounds
         }
         userScore = 0;
         computerScore = 0;
+        roundCount = 0;
     }
 }
 //game round is triggered by user input
